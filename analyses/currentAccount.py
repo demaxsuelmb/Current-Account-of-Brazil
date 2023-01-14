fe6f1457e467ac9cef034bdab25d057c0b1e58c6
Demaxsuel Batista <92854524+demaxsuelmb@users.noreply.github.com>
2023-01-14 18:27:28 -0300

    processDatas

--- analyses/currentAccount.py
+++ analyses/currentAccount.py
@@ -3,51 +3,39 @@
 import pandas as pd ## library to read files
 import numpy as np ## library to process datas
 
-
 ## Import files to analyses
 
 # CurrentAccount
 # The location of the file which is to be retrieved using this function.
 currenteAccount_url = "https://raw.githubusercontent.com/demaxsuelmb/Current-Account-of-Brazil/main/bases/balanca%20comercial.csv"
-
 # It stands for separator
 sep_ca = ';'
-
 df_ca = pd.read_csv(currenteAccount_url, sep=sep_ca)
 
 
 # PrimaryIncome
 # The location of the file which is to be retrieved using this function.
 primaryIncome_url = "https://raw.githubusercontent.com/demaxsuelmb/Current-Account-of-Brazil/main/bases/rendaPrimaria.csv"
-
 # It stands for separator
 sep_pi = ';'
-
 df_pi = pd.read_csv(primaryIncome_url, sep=sep_pi)
-
-print(df_pi)
-
-
 
 # SecondIncome
 # The location of the file which is to be retrieved using this function.
-secondIncome_url = ""
-
+secondIncome_url = "https://raw.githubusercontent.com/demaxsuelmb/Current-Account-of-Brazil/main/bases/rendaSecundaria.csv"
 # It stands for separator
 sep_si = ';'
-
-df_pi = pd.read_csv(currenteAccount_url, sep=sep_si)
+df_si = pd.read_csv(secondIncome_url, sep=sep_si)
 
 
 # Services
 # The location of the file which is to be retrieved using this function.
-services_url = ""
-
+services_url = "https://raw.githubusercontent.com/demaxsuelmb/Current-Account-of-Brazil/main/bases/servicos.csv"
 # It stands for separator
 sep_s = ';'
-
-df_s = pd.read_csv(currenteAccount_url, sep=sep_s)
-
+df_s = pd.read_csv(services_url, sep=sep_s)
 
 
-https://github.com/demaxsuelmb/Current-Account-of-Brazil/blob/main/bases/rendaPrimaria.csv
+
+
+
