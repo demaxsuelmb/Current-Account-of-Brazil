bc956415d29778429538b45ea38b67be942d296f
Demaxsuel Batista <92854524+demaxsuelmb@users.noreply.github.com>
2023-01-14 17:56:47 -0300

    renameFiles

--- analyses/currentAccount.py
+++ analyses/currentAccount.py
@@ -3,6 +3,51 @@
 import pandas as pd ## library to read files
 import numpy as np ## library to process datas
 
-df = pd.read_csv("https://github.com/demaxsuelmb/Current-Account-of-Brazil/blob/main/bases/balanca comercial.csv")
 
-print(df)
+## Import files to analyses
+
+# CurrentAccount
+# The location of the file which is to be retrieved using this function.
+currenteAccount_url = "https://raw.githubusercontent.com/demaxsuelmb/Current-Account-of-Brazil/main/bases/balanca%20comercial.csv"
+
+# It stands for separator
+sep_ca = ';'
+
+df_ca = pd.read_csv(currenteAccount_url, sep=sep_ca)
+
+
+# PrimaryIncome
+# The location of the file which is to be retrieved using this function.
+primaryIncome_url = "https://github.com/demaxsuelmb/Current-Account-of-Brazil/blob/main/bases/rendaPrimaria.csv"
+
+# It stands for separator
+sep_pi = ';'
+
+df_pi = pd.read_csv(primaryIncome_url, sep=sep_pi)
+
+print(df_pi)
+
+
+
+# SecondIncome
+# The location of the file which is to be retrieved using this function.
+secondIncome_url = ""
+
+# It stands for separator
+sep_si = ';'
+
+df_pi = pd.read_csv(currenteAccount_url, sep=sep_si)
+
+
+# Services
+# The location of the file which is to be retrieved using this function.
+services_url = ""
+
+# It stands for separator
+sep_s = ';'
+
+df_s = pd.read_csv(currenteAccount_url, sep=sep_s)
+
+
+
+https://github.com/demaxsuelmb/Current-Account-of-Brazil/blob/main/bases/rendaPrimaria.csv
