bb04938c49a058e268f2ae962a0e6f46d31f5ef0
Demaxsuel Batista <92854524+demaxsuelmb@users.noreply.github.com>
2023-01-14 17:26:39 -0300

    renameFiles

--- analyses/currentAccount.py
+++ analyses/currentAccount.py
@@ -1,4 +1,8 @@
-# instalando as bibliotecas
+# installing the libraries to processing and analyses
 
-import pandas as pd
-import numpy as np
+import pandas as pd ## library to read files
+import numpy as np ## library to process datas
+
+df = pd.read_csv("https://github.com/demaxsuelmb/Current-Account-of-Brazil/blob/main/bases/balanca comercial.csv")
+
+print(df)
