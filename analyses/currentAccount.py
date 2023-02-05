aec90dfe76043c731603847cb18b1a24756772e5
Demaxsuel Batista <92854524+demaxsuelmb@users.noreply.github.com>
2023-02-04 22:24:06 -0300

    merged_and_conbine

--- analyses/currentAccount.py
+++ analyses/currentAccount.py
@@ -14,7 +14,14 @@ sep_ca = ';'
 df_ca = pd.read_csv(currenteAccount_url, sep=sep_ca)
 
 # print the data to verify if the data was correct
-print("currentAccount")
+print("currentAccount df")
+print("shape and df")
+print(df_ca.shape)
+
+#rename columns
+df_ca =  df_ca.rename(columns={'data': 'date', 'valor': 'currentAccount'})
+print(df_ca.head())
+
 
 # PrimaryIncome
 # The location of the file which is to be retrieved using this function.
@@ -24,7 +31,13 @@ sep_pi = ';'
 df_pi = pd.read_csv(primaryIncome_url, sep=sep_pi)
 
 # print the data to verify if the data was correct
-print("primaryIncome")
+print("primaryIncome df")
+print("shape and df")
+print(df_pi.shape)
+
+#rename columns
+df_pi = df_pi.rename(columns={"data":"date", "valor":"primaryIncome"})
+print(df_pi.head())
 
 # SecondIncome
 # The location of the file which is to be retrieved using this function.
@@ -34,8 +47,13 @@ sep_si = ';'
 df_si = pd.read_csv(secondIncome_url, sep=sep_si)
 
 # print the data to verify if the data was correct
-print("secondIncome")
+print("secondIncome df")
+print("shape and df")
+print(df_si.shape)
 
+#rename columns
+df_si = df_si.rename(columns={"data":"date", "valor": "secondIncome"})
+print(df_si.head())
 
 # Services
 # The location of the file which is to be retrieved using this function.
@@ -45,9 +63,24 @@ sep_s = ';'
 df_s = pd.read_csv(services_url, sep=sep_s)
 
 # print the data to verify if the data was correct 
-print("service")
+print("service df")
+print("shape and df")
+print(df_s.shape)
 
-# rename columns
-df_ca =  df_ca.rename(columns={'data': 'date', 'valor': 'currentAccount'})
+#rename columns
+df_s = df_s.rename(columns={"data":"date", "valor":"services"})
+print(df_s.head())
+
 
 # merge dataframes to a only df
+# merge primaryIncome
+df = pd.merge(df_ca, df_pi, how="left", on=["date"])
+print(df.head())
+
+# merge secondIncome
+df = pd.merge(df_ca, df_si, how="left", on=["date"])
+print(df.head())
+
+# merge service
+df = pd.merge(df_ca, df_s, how="left", on=["date"])
+print(df.head())
